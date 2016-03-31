2ff7df04501166bca8203663877ee7c92c662355
timoxa9510 <dimon-andronov@yandex.ru>
2016-04-01 00:19:11 +0400

    module1-task2

---
+++ build/js/check.js
@@ -0,0 +1,29 @@
+function getMessage(a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров'}
+    else {
+      return 'Переданное GIF-изображение не анимировано';
+    }
+  }
+
+  if (typeof a === 'number') {
+     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+  }
+
+  if (typeof a === 'object' && typeof b !== 'object') {
+    var sum = 0;
+    for (var i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+    return 'Количество красных точек во всех строчках изображения: ' + sum;
+  }
+
+  if (typeof a === 'object' && typeof b === 'object') {
+		var square = 0;
+		for (var i = 0; i < a.length; i++) {
+		square += a[i] * b[i];
+	}
+    return 'Общая площадь артефактов сжатия: ' + square+ ' пикселей'
+  }
+}
